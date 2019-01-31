47561738e6bd0fb91d07bcf04e24dce904d14892
jetzable <gjetzable@gmail.com>
2019-01-30 18:03:49 -0600

    Example

---
+++ index.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const app = express();
+const express_graphql = require('express-graphql');
+const { buildSchema } = require('graphql');
+const { courses } = require('./data.json');
+
+
+// Building Schema
+const schema = buildSchema(`
+type Query {
+    course(id: Int!): Course
+    coursesByTopic(topic: String!): [Course]
+}
+
+type Mutation{
+  updateCourseTopic(id: Int!, topic: String!): Course
+  addCourseTopic(id: Int!, topic: String!): Course
+}
+
+type Course {
+    id: Int
+    title: String
+    description: String
+    author: String
+    topics: [Topic]
+    url: String
+}
+
+type Topic{
+  id: Int
+  title: String
+}
+
+`)
+
+
+// Queries Methods
+const getCourse = (args) => {
+  const id = args.id;
+  return courses.filter(course => {
+    return course.id === id
+  })[0]
+}
+
+const getCoursesByTopic = ({ topic }) => {
+  courses.map(course => {
+    course.topics.forEach(topicElement => {
+      if (topicElement.title === topic) {
+        return course;
+      }
+    })
+  })
+}
+
+const updateCourseTopic = ({ id, topicId, topic }) => {
+  courses.map(course => {
+    if (course.id === id) {
+      course.topics.map(topicElement => {
+        if (topicId === topicElement.id) {
+          topicElement.title = topic;
+          return course;
+        }
+      })
+    }
+  })
+  return courses.filter(course => course.id === id)[0];
+}
+
+
+const addCourseTopic = ({ id, topicTitle, topicId }) => {
+  const newTopic = {
+    topicId,
+    topicTitle
+  }
+  courses.map(course => {
+    if (course.id === id) {
+      course.topic.push(newTopic);
+      return course;
+    }
+  })
+  return courses.filter(course => course.id === id)[0];
+}
+
+const root = {
+  course: getCourse,
+  coursesByTopic: getCoursesByTopic,
+  updateCourseTopic: updateCourseTopic,
+  addCourseTopic: addCourseTopic
+}
+
+app.use('/graphql', express_graphql({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}))
+
+app.listen(5000, () => console.log('Server Listening'));
